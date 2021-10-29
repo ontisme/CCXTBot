ecfb6ddc971abb5ab75f879ede7a74b688baa3ec
lyfx <lyfxme@gmail.com>
2021-10-29 13:12:01 +0800

    update

--- app.py
+++ app.py
@@ -206,4 +206,5 @@ if __name__ == "__main__":
     # }
     #
     # print(a)
-    app.run(host="0.0.0.0", port=int(os.environ["PORT"]))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
